b16a11971d13a69bf598cd9ab750e8f4ed5db4e9
gmlxo <87000854+gmlxo@users.noreply.github.com>
2022-11-01 18:55:08 +0900

    search

--- front/js/menu-script.js
+++ front/js/menu-script.js
@@ -2,8 +2,8 @@ $("#ser").click(function (e) {
     alert('수민이 지원');    
 });
 
-$(".search-box").hover(function(){
-    $(".search-box input[type=submit]").css("background-color", "rgb(51, 51, 217)");
+$(".search-box-popUp").hover(function(){
+    $(".search-box-popUp input[type=submit]").css("background-color", "rgb(51, 51, 217)");
 }, function(){
-    $(".search-box input[type=submit]").css("background-color", "");
+    $(".search-box-popUp input[type=submit]").css("background-color", "");
 });
