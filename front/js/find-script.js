56891eaf75cc37e9fcd73201a90409d028480707
gmlxo <87000854+gmlxo@users.noreply.github.com>
2022-11-09 12:50:31 +0900

    .

---
+++ front/js/find-script.js
@@ -0,0 +1,34 @@
+$(".tab-news").click(function(){
+    $(this).css({
+        "border-bottom" : "0",
+        "background-color" : "white"
+    });
+    $(".tab-gallery").css({
+        "border-bottom" : "1px solid #000",
+        "background-color" : "#888"
+    });
+    $(".gallery").hide();
+    $(".news").show();
+});
+
+$(".tab-gallery").click(function(){
+    $(this).css({
+        "border-bottom" : "0",
+        "background-color" : "white"
+    });
+    $(".tab-news").css({
+        "border-bottom" : "1px solid #000",
+        "background-color" : "#888"
+    });
+
+    $(".news").hide();
+    $(".gallery").show();
+});
+
+function openPop() {
+    $("#pop").show("slow");
+}
+
+function closePop() {
+    $("#pop").hide("fast");
+}
