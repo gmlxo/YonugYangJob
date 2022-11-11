e649e458ac75bb2ba4a9de7779f06913b8692bbf
gmlxo <87000854+gmlxo@users.noreply.github.com>
2022-11-11 15:17:32 +0900

    back reset

--- YonugYangJob/front/js/menu-script.js
+++
@@ -1,9 +0,0 @@
-$("#ser").click(function (e) { 
-    alert('수민이 지원');    
-});
-
-$(".search-box-popUp").hover(function(){
-    $(".search-box-popUp input[type=submit]").css("background-color", "rgb(51, 51, 217)");
-}, function(){
-    $(".search-box-popUp input[type=submit]").css("background-color", "");
-});
