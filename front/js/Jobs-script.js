310c6d817d186003ecaa6a5b0fae634bf53306e0
gmlxo <87000854+gmlxo@users.noreply.github.com>
2022-11-11 22:53:02 +0900

    혼남

--- front/js/Jobs-script.js
+++ front/js/Jobs-script.js
@@ -1,26 +1,33 @@
 /* 지역 */
+var region = [".seoul", ".gyeonggi", ".incheon", ".busan", ".daegu", ".gwangju", ".daejeon", ".ulsan", ".sejong", ".gangwon", ".gyeongnam", ".gyeongbuk", ".chungnam", ".jeju", ".national"]
 
-$(".region-jobs").click(function(){
+/* 행정구역 */
+var seoul = [".all_seoul", ".gangnam", ".gangdong", ".ganbuk", ".gangseo", ".gwanak", ".gwangjin", ".guro", ".geumcheon", ".nowon", ".dobong", ".dongdaemun", ".otherthan-seoul"];
+var gyeonggi = [".all_gyeonggi", ".SouthernGyeonggi", ".suwon", ".yongin", ".seongnam", ".bucheon", ".hwaseongCity", ".ansan", ".anyang", ".pyeongtaek", ".siheung", ".gimpoCity", ".gwangju", ".gwangmyeong", ".gunpo", ".hanam", ".osan", ".icheon", ".anseong", ".uiwang", ".yangpyeong", ".yeoju", ".otherthan-seoul"];
+var incheon = [".ganghwa", ".ongjin", ".jung_in", ".dong_in", ".michuholgu", ".yeonsu", ".namdong", ".bupyeong", ".gyeyang", ".western"];
+var busan = [".jung", ".dong"];
+ 
+$(".region-jobs").click(function () {
     $(this).css({
-        "border-bottom" : "0",
-        "background-color" : "white"
+        "border-bottom": "0",
+        "background-color": "white"
     });
     $(".Job-jobs").css({
-        "border-bottom" : "1px solid #000",
-        "background-color" : "#888"
+        "border-bottom": "1px solid #000",
+        "background-color": "#888"
     });
     $(".test").hide();
     $(".region-box").show();
 });
 
-$(".Job-jobs").click(function(){
+$(".Job-jobs").click(function () {
     $(this).css({
-        "border-bottom" : "0",
-        "background-color" : "white"
+        "border-bottom": "0",
+        "background-color": "white"
     });
     $(".region-jobs").css({
-        "border-bottom" : "1px solid #000",
-        "background-color" : "#888"
+        "border-bottom": "1px solid #000",
+        "background-color": "#888"
     });
 
     $(".region-box").hide();
