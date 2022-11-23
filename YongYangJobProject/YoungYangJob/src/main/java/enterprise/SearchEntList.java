b35563b6bdff108dcf3763da081eb4b46c483080
gmlxo_ <87000854+gmlxo@users.noreply.github.com>
2022-11-24 02:06:41 +0900

    Update SearchEntList.java

--- YongYangJobProject/YoungYangJob/src/main/java/enterprise/SearchEntList.java
+++ YongYangJobProject/YoungYangJob/src/main/java/enterprise/SearchEntList.java
@@ -23,16 +23,21 @@ public class SearchEntList extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EnterpriseDAO dao = new EnterpriseDAO();
 		
+        /* n keyword 값 받아오기 */
 		String n = request.getParameter("n");
 		String keyword = request.getParameter("keyword");
 		
+        /* 조건에 맞는 값의 개수 가져오기 */
 		int count = dao.allCountEnt(n, keyword);
+        /* 조건에 맞는 값 받아와 list에 담기 */
 		ArrayList<EnterpriseVO> list = dao.EntDateList(n, keyword);
 		
+        /* 값이 있으면 각각 맞게 저장 */
 		if(list != null) {
 			request.setAttribute("count", count);
 			request.setAttribute("entList", list);
 		}
+        /* enterprise jsp 로 이동 */
 		request.getRequestDispatcher("/Information/enterprise.jsp").forward(request, response);
 	}
 }
