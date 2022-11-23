d204b5474514f57407976386ee2f1b9612809c23
gmlxo_ <87000854+gmlxo@users.noreply.github.com>
2022-11-24 01:41:33 +0900

    Update EmploymentDAO.java

--- YongYangJobProject/YoungYangJob/src/main/java/dao/EmploymentDAO.java
+++ YongYangJobProject/YoungYangJob/src/main/java/dao/EmploymentDAO.java
@@ -52,6 +52,7 @@ public class EmploymentDAO {
 		return empList;
 	}
 
+// 채용 공고 올리기
 	public int insertEmp(String emp_title, String emp_name, String emp_email, String company_name, String emp_url,
 			String career, String education, String work_type, String emp_day, String emp_contents) {
 		int n = 0;
@@ -81,7 +82,7 @@ public class EmploymentDAO {
 		return n;
 	}
 
-//	index 의 필요한 값들을 가져온다.
+//	채용 공고들 불러 오기 
 	public ArrayList<Ent_emp_VO> indexEntList() {
 		ArrayList<Ent_emp_VO> indexList = new ArrayList<Ent_emp_VO>();
 		sql = "SELECT emp.company_name, emp.emp_title, TO_DATE(emp.emp_day, 'YYYY-MM-DD') - TO_DATE(sysdate, 'YYYY-MM-DD') emp_day, substr(emp.emp_contents, 1, 20) emp_contents, ent.logo_img, ent.sectors FROM employment_tbl_gmlxo emp, enterprise_tbl_gmlxo ent where emp.company_name = ent.ent_name";
@@ -114,6 +115,7 @@ public class EmploymentDAO {
 		return indexList;
 	}
 
+// 지역에 맞는 채용 공고 가져오기
 	public ArrayList<Ent_emp_VO> seaJobList(String keyword) {
 		ArrayList<Ent_emp_VO> jobList = new ArrayList<Ent_emp_VO>();
 		sql = "SELECT emp.company_name, emp.emp_title, TO_DATE(emp.emp_day, 'YYYY-MM-DD') - TO_DATE(sysdate, 'YYYY-MM-DD') emp_day, substr(emp.emp_contents, 1, 70) emp_contents, ent.logo_img, ent.sectors FROM employment_tbl_gmlxo emp, enterprise_tbl_gmlxo ent where emp.company_name = ent.ent_name and ent.address like ?";
@@ -148,6 +150,7 @@ public class EmploymentDAO {
 		return jobList;
 	}
 	
+// 제목에 맞는 채용 공고 가져오기
 	public ArrayList<Ent_emp_VO> annEntEmpList(String title) {
 		ArrayList<Ent_emp_VO> annList = new ArrayList<Ent_emp_VO>();
 		sql = "select ent.sectors, ent.address, ent.url ent_url, emp.emp_title, emp.emp_name, emp.emp_email, emp.company_name, TO_DATE(emp.emp_day, 'YYYY-MM-DD') - TO_DATE(sysdate, 'YYYY-MM-DD') emp_day, emp.emp_url, decode(emp.career, '1','무관', '2', '~ 2년', '5','~ 5년', '8','~ 10년', '9', '10 ~') emp_career, decode(emp.education, '1','고졸', '2','2년제', '4','4년제', '5', '대학원') emp_education, decode(emp.work_type, '1','비정규직', '2', '정규직') emp_work_type, emp.emp_contents from employment_tbl_gmlxo emp, enterprise_tbl_gmlxo ent where emp.company_name = ent.ent_name and emp.emp_title = ?";
