7785778b27343dee4f865957fca1880d3f38f4de
gmlxo_ <87000854+gmlxo@users.noreply.github.com>
2022-11-24 02:04:06 +0900

    Update EntList.java

--- YongYangJobProject/YoungYangJob/src/main/java/enterprise/EntList.java
+++ YongYangJobProject/YoungYangJob/src/main/java/enterprise/EntList.java
@@ -25,16 +25,21 @@ public class EntList extends HttpServlet {
 
 		EnterpriseDAO dao = new EnterpriseDAO();
 
+		/* n keyword 값 받아오기 */
 		String n = request.getParameter("n");
 		String keyword = request.getParameter("keyword");
 
+		/* 조건에 맞는 개수 받아오기 */
 		int count = dao.allCountEnt(n, keyword);
+		/* 조건에 맞는 값 받아와 list에 담기 */
 		ArrayList<EnterpriseVO> list = dao.EntDateList(n, keyword);
 
-		if (list != null) {
+		/* 값이 있다먄 각각 맞게 저장 */
+		if (list != null count < 0) {
 			request.setAttribute("count", count);
 			request.setAttribute("entList", list);
 		}
+		/* enterprise jsp 로 이동 */
 		request.getRequestDispatcher("/Information/enterprise.jsp").forward(request, response);
 	}
 }
