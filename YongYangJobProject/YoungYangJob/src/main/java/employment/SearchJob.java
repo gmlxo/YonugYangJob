bd5e9f0724fc04d38c158311325ceff40e7b4560
gmlxo_ <87000854+gmlxo@users.noreply.github.com>
2022-11-24 01:52:41 +0900

    Update SearchJob.java

--- YongYangJobProject/YoungYangJob/src/main/java/employment/SearchJob.java
+++ YongYangJobProject/YoungYangJob/src/main/java/employment/SearchJob.java
@@ -27,12 +27,16 @@ public class SearchJob extends HttpServlet {
 			throws ServletException, IOException {
 		EmploymentDAO dao = new EmploymentDAO();
 		
+		/* keyword 값 받아오기 */
 		String keyword = request.getParameter("keyword");
+		/* keyword 에 맞는 값의 정보를 list에 저장하기 */
 		ArrayList<Ent_emp_VO> list = dao.seaJobList(keyword);
 
+		/* null 이 아니면 list 저장 */
 		if (list != null) {
 			request.setAttribute("indexList", list);
 		}
+		/* jobs jsp 로 이동 */
 		request.getRequestDispatcher("/Information/jobs.jsp").forward(request, response);
 	}
 }
